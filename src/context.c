070261d5d11bed8e4fb1fa1684e87a5676d48459
KunalDesai24 <kunaldesai200124@gmail.com>
2025-11-14 10:12:24 +0530

    Implement Create Context

---
+++ src/context.c
@@ -0,0 +1,27 @@
+#include "context.h"
+#include <stdio.h>
+
+
+void context_create(thread_t *thread, void (*func)(void *), void *arg)
+{
+    if(getcontext(&thread->context) == -1)
+    {
+        perror("Getcontext failed");
+        exit(1);
+    }
+    thread->stack = malloc(STACK_SIZE);
+    if(!thread->stack)
+    {
+        perror("Malloc Failed");
+        exit(1);
+    }
+    thread->context.uc_stack.ss_sp = thread->stack;
+    thread->context.uc_stack.ss_size = STACK_SIZE;
+    thread->context.uc_link = NULL;
+    makecontext(&thread->context, (void (*)(void))func, 1, arg);
+}
+
+void context_switch(thread_t *from, thread_t *to)
+{
+    swapcontext(&from->context, &to->context);
+}
