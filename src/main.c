0c5de0925ec86fa59b2aa0f72da9a7884ff4a4b3
KunalDesai24 <kunaldesai200124@gmail.com>
2025-11-14 17:30:23 +0530

    Update main()

--- src/main.c
+++ src/main.c
@@ -5,22 +5,23 @@
 void func(void *arg)
 {
     int id = *(int *)arg;
-    printf("Thread %d is running\n",id);
+    for(int i=0; i<5; i++)
+    {
+        printf("Thread %d is running and i = %d\n",id,i);
+        thread_yield();
+    }
+    thread_exit();
 }
 int main() 
 { 
     int id1 = 1;
     int id2 = 2;
+    int id3 = 3;
 
     thread_create(func, &id1, 1);
     thread_create(func, &id2, 1);
+    thread_create(func, &id3, 1);
 
-    thread_t *thread;
-    while((thread = scheduler_pick_next_thread()) != NULL)
-    {
-        thread->state = RUNNING;
-        setcontext(&thread->context);
-    }
-    printf("All thread finish\n"); 
+    thread_yield();
     return 0; 
 }
