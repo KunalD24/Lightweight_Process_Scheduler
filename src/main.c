018da3cb7301c9f27e3ee9c7a23622f64e7d3d4a
KunalDesai24 <kunaldesai200124@gmail.com>
2025-11-15 23:17:23 +0530

    Update main()

--- src/main.c
+++ src/main.c
@@ -4,19 +4,28 @@
 #include "timer.h"
 #include <string.h>
 
+int preemptive = 0;
+
 void func(void *arg)
 {
     int id = *(int *)arg;
-    for(int i=0; i<5; ++i)
+    for(int i = 0; i < 5; ++i)
     {
-        printf("Thread %d is running and i = %d\n",id,i);
-        thread_yield();
+        printf("Thread %d is running and i = %d\n", id, i);
+
+        for (volatile long j = 0; j < 50000000; j++);
+
+        if (!preemptive)
+        {
+            thread_yield();
+        }
     }
     thread_exit();
 }
+
 int main(int argc, char **argv) 
 { 
-    int preemptive = 0;
+    preemptive = 0;
     if(argc > 1)
     {
         if(strcmp(argv[1],"--mode=preemptive") == 0)
@@ -38,14 +47,18 @@ int main(int argc, char **argv)
     int id2 = 2;
     int id3 = 3;
 
-    thread_create(func, &id1, 1);
-    thread_create(func, &id2, 1);
-    thread_create(func, &id3, 1);
+    int tid1 = thread_create(func, &id1, 3);
+    int tid2 = thread_create(func, &id2, 1);
+    int tid3 = thread_create(func, &id3, 2);
 
     if(preemptive) 
     {
         timer_init();
     }
     thread_yield();
+
+    thread_join(tid1);
+    thread_join(tid2);
+    thread_join(tid3);
     return 0; 
 }
