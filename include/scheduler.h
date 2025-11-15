c3bc354bfe11025d06927288916e5e17991a7e12
KunalDesai24 <kunaldesai200124@gmail.com>
2025-11-15 23:08:28 +0530

    Update Scheduler Header

--- include/scheduler.h
+++ include/scheduler.h
@@ -6,7 +6,7 @@
 extern thread_t *ready_queue;
 extern thread_t *all_thread;
 
-void scheduler_add_thread();
-thread_t* scheduler_pick_next_thread();
-void scheduler_remove_thread();
+void scheduler_add_thread(thread_t *thread);
+thread_t* scheduler_pick_next_thread(void);
+void scheduler_remove_thread(thread_t *thread);
 #endif
